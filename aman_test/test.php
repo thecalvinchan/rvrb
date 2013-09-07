22b88366897cb42776ed19ea39e52f36585c39b8
Aman Agarwal <amanaamazing@gmail.com>
2013-09-06 22:57:36 -0700

    ehwq

--- aman_test/test.php
+++ aman_test/test.php
@@ -2,10 +2,10 @@
 require_once 'php-soundcloud/Services/Soundcloud.php';
 
 // create client object with app credentials
-$client = new Services_Soundcloud(
-    'd42ba4a95dc468d30b8683e9956e430e', '057c884f3b17bfb0ceaddad026cde32b', 'http://localhost/test2.php');
+// $client = new Services_Soundcloud(
+//    'd42ba4a95dc468d30b8683e9956e430e', '057c884f3b17bfb0ceaddad026cde32b', 'http://localhost/test2.php');
 
-$authorizeUrl = $client->getAuthorizeUrl();
+// $authorizeUrl = $client->getAuthorizeUrl();
 
 ?>
 <html>

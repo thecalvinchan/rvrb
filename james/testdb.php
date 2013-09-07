73d2ef201c04e2915bb936fc3761dd24bed29c61
jwu <jwu@efunda.com>
2013-09-07 13:44:34 -0400

    insert

--- james/testdb.php
+++ james/testdb.php
@@ -8,6 +8,11 @@ $db = substr($url["path"],1);
 $connection = mysql_connect($server, $username, $password);
 mysql_select_db($db);
 
+$q = mysql_query('DESCRIBE rvrb_users');
+while($row = mysql_fetch_array($q)) {
+    echo "{$row['Field']} - {$row['Type']}\n";
+}
+
 $sql = "INSERT INTO rvrb_users (id) VALUES (11111111)";
 if (!mysql_query($connection, $sql)) {
 	echo ('everything is terrible');

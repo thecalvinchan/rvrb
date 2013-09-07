a7b0ace9710b3b4e12422efbf6d87cd2777ef687
Aman Agarwal <amanaamazing@gmail.com>
2013-09-07 10:33:21 -0700

    auth2auth

--- auth.php
+++ auth.php
@@ -15,7 +15,7 @@ if (isset($type)) {
     header("Location: " . $url);
   }
 } elseif (isset($code)) {
-  $user_json = $client->get('me');
+  $user_json = $client->get('/me');
 //  $user = json_decode($client->get('me'));
 //  var_dump($user);
 }

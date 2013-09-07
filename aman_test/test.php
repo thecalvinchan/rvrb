84a925799497699b56632e22cacc43122025accd
Aman Agarwal <amanaamazing@gmail.com>
2013-09-06 23:11:17 -0700

    sc api

--- aman_test/test.php
+++ aman_test/test.php
@@ -3,7 +3,7 @@ require_once 'php-soundcloud/Services/Soundcloud.php';
 
 // create client object with app credentials
 $client = new Services_Soundcloud(
-    'd42ba4a95dc468d30b8683e9956e430e', '057c884f3b17bfb0ceaddad026cde32b', 'http://localhost/test2.php');
+    'd42ba4a95dc468d30b8683e9956e430e', '057c884f3b17bfb0ceaddad026cde32b', 'http://rvrb.herokuapp.com/aman_test/test2.php');
 
 $authorizeUrl = $client->getAuthorizeUrl();
 

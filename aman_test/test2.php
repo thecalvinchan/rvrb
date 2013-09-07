8a805480d721acf7dac01967bc664435b470caa4
Aman Agarwal <amanaamazing@gmail.com>
2013-09-06 23:33:55 -0700

    sc api

--- aman_test/test2.php
+++ aman_test/test2.php
@@ -6,16 +6,10 @@ $client = new Services_Soundcloud(
     'd42ba4a95dc468d30b8683e9956e430e', '057c884f3b17bfb0ceaddad026cde32b',
     'http://rvrb.herokuapp.com/aman_test/test2.php');
 
-// var_dump(get_class_methods($client));
-// $code = $_GET['code'];
-// $accessToken = $client->accessToken($code);
-// var_dump($accessToken);
-
 try {
-      $accessToken = $client->accessToken($_GET['code']);
-      var_dump($accessToken);
+      $token_arr = $client->accessToken($_GET['code']);
+      var_dump($token_arr);
 } catch (Services_Soundcloud_Invalid_Http_Response_Code_Exception $e) {
-      echo 'b';
       echo $e->getMessage();
 }
 

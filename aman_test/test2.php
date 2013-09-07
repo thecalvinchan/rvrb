02e631c7789c8df0683b95805b95d7c22ec1b300
jwu <jwu@efunda.com>
2013-09-07 12:38:10 -0400

    testing

--- aman_test/test2.php
+++ aman_test/test2.php
@@ -2,6 +2,7 @@
 require_once 'php-soundcloud/Services/Soundcloud.php';
 
 // let's connect to the database
+try {
 $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 $server = $url["host"];
 $username = $url["user"];
@@ -10,7 +11,7 @@ $db = substr($url["path"],1);
 
 mysql_connect($server, $username, $password);
 mysql_select_db($db);
-
+} catch (Exception $e) { echo $e->getMessage(); }
 // create client object with app credentials
 $client = new Services_Soundcloud(
     'd42ba4a95dc468d30b8683e9956e430e', '057c884f3b17bfb0ceaddad026cde32b',

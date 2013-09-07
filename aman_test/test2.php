e6326c028a2c542bf627159de39357f2402e4f32
Aman Agarwal <amanaamazing@gmail.com>
2013-09-06 23:21:16 -0700

    sc api

--- aman_test/test2.php
+++ aman_test/test2.php
@@ -6,7 +6,7 @@ $client = new Services_Soundcloud(
     'd42ba4a95dc468d30b8683e9956e430e', '057c884f3b17bfb0ceaddad026cde32b', 'http://localhost/test2.php');
 
 // var_dump(get_class_methods($client));
-// $accessToken = $client->accessToken($_GET['code']);
+$accessToken = $client->accessToken($_GET['code']);
 $code = $_GET['code'];
 // $accessToken = $client->accessToken($code);
 // var_dump($accessToken);

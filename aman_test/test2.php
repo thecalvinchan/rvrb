9d519c9aa8e5bc9ec669161a635fbd81c0b95d2f
Aman Agarwal <amanaamazing@gmail.com>
2013-09-06 23:25:01 -0700

    sc api

--- aman_test/test2.php
+++ aman_test/test2.php
@@ -3,21 +3,21 @@ require_once 'php-soundcloud/Services/Soundcloud.php';
 
 // create client object with app credentials
 $client = new Services_Soundcloud(
-    'd42ba4a95dc468d30b8683e9956e430e', '057c884f3b17bfb0ceaddad026cde32b', 'http://localhost/test2.php');
+    'd42ba4a95dc468d30b8683e9956e430e', '057c884f3b17bfb0ceaddad026cde32b',
+    'http://localhost/test2.php');
 
 // var_dump(get_class_methods($client));
-$code = $_GET['code'];
-$accessToken = $client->accessToken($code);
+// $code = $_GET['code'];
+// $accessToken = $client->accessToken($code);
 // var_dump($accessToken);
 //
 
-/*
 try {
       $accessToken = $client->accessToken($_GET['code']);
+      echo $accessToken;
 } catch (Services_Soundcloud_Invalid_Http_Response_Code_Exception $e) {
-      exit($e->getMessage());
+      echo $e->getMessage();
 }
- */
 
 ?>
 <html>

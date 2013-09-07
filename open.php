30de54a719bc6802457019f2c9901a8a0287dc9e
jwu <jwu@efunda.com>
2013-09-07 16:54:52 -0400

    prettify open page

---
+++ open.php
@@ -0,0 +1,37 @@
+<?php
+?>
+<!DOCTYPE html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang="en">
+<![endif]-->
+  <!--[if gt IE 8]><!-->
+  <html class="no-js" lang="en">
+    <!--<![endif]-->
+    <head>
+      <meta charset="utf-8" />
+      <!-- Set the viewport width to device width for mobile -->
+      <meta name="viewport" content="width=device-width" />
+      <title>Rvrb</title>
+      <!-- Included CSS Files (Compressed) -->
+      <link rel="stylesheet" href="style-open.css" />
+      <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+    </head>
+    <body class="splash">
+      <div class="row">
+        <div id="mixbuttoncontainer">
+          <div class="four columns openmixbutton">
+            <h3>No files are open.</h3>
+            <h4>Open a file to begin editing.</h4>
+          </div><br>
+          <div class="four columns openmixbutton">
+            <h5><a href="#signup"><img src="img/files.png" />&nbsp;&nbsp;New mix</a></h5>
+          </div>
+          <div class="four columns openmixbutton">
+            <h5><a href="#about"><img src="img/load.png" />&nbsp;&nbsp;Load mix</a></h5>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>

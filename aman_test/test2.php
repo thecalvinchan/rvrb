d96cfb99a1906421d2a7dda70ca747b6fc7af06e
Aman Agarwal <amanaamazing@gmail.com>
2013-09-07 00:41:31 -0700

    sc api upload file

--- aman_test/test2.php
+++ aman_test/test2.php
@@ -6,20 +6,20 @@ $client = new Services_Soundcloud(
     'd42ba4a95dc468d30b8683e9956e430e', '057c884f3b17bfb0ceaddad026cde32b',
     'http://rvrb.herokuapp.com/aman_test/test2.php');
 
-try {
-    $token_arr = $client->accessToken($_GET['code']);
-    $access = $token_arr["access_token"];
-    var_dump($token_arr);
-    var_dump($access);
-    var_dump($token_arr["refresh_token"]);
-} catch (Services_Soundcloud_Invalid_Http_Response_Code_Exception $e) {
-    echo $e->getMessage();
-}
+$token_arr = $client->accessToken($_GET['code']);
+$access = $token_arr["access_token"];
+var_dump($token_arr);
+var_dump($access);
+var_dump($token_arr["refresh_token"]);
 
-$track = json_decode($client->post('tracks', array(
+$track_data = array(
   'track[title]' => 'super appropriate song',
   'track[asset_data]' => 'super_appropriate_song.mp3'
-)));
+);
+
+var_dump($track_data);
+
+$track = json_decode($client->post('tracks', $track_data));
 
 var_dump($track);
 
